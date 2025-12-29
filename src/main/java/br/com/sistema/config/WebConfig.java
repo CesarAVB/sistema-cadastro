796cdfee96929bd57b39763b0ad43cada1a06b0e
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2025-12-28 21:25:34 -0300

    ajuste de configuração de cors e security para permitir Coolify

--- src/main/java/br/com/sistema/config/WebConfig.java
+++ src/main/java/br/com/sistema/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200",
-                				"https://frontend-syscadastro.vercel.app")
+                				"https://syscadastro.cesaravb.com.br")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
