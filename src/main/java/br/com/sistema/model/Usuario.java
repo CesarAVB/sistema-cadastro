ef41906415ceb6e603dcdebba9291b3b7b261527
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-01-01 23:58:09 -0300

    feat(usuario): adiciona suporte a foto de perfil e tema de interface
    - Adiciona campo fotoPerfil como @Lob para armazenamento em base64
    - Adiciona campo tema com valor padrão 'light'
    - Inclui getters e setters para os novos campos

--- src/main/java/br/com/sistema/model/Usuario.java
+++ src/main/java/br/com/sistema/model/Usuario.java
@@ -4,10 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
